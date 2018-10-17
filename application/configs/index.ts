f16212f61f104a1f621182ea9677ee3c3a3e2cf9
ranyunlong <549510622@qq.com>
2018-10-17 19:44:06 +0800

    test

--- application/configs/index.ts
+++ application/configs/index.ts
@@ -1,9 +1,13 @@
 import { Server } from '@longjs/server';
 import { RedisStorage } from '@longjs/redis-storage'
+import { resolve } from 'path'
 
 export const configs: Server.Configs = {
     session: {
         store: new RedisStorage(),
         signed: true
+    },
+    staticServeOpts: {
+        root: resolve('public')
     }
 }

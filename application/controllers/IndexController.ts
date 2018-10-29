6dfdfd441236c6db2228ff416dafe4bbddbdcf81
ranyunlong <549510622@qq.com>
2018-10-29 18:49:16 +0800

    update

--- application/controllers/IndexController.ts
+++ application/controllers/IndexController.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@longjs/decorators'
+import { Controller, Get, Session } from '@longjs/decorators'
 
 @Controller('/')
 export class IndexController {
-
+    @Session public session: Session;
     @Get('/')
     async index() {
+        if (this.session.user) {
+            return this.session.user
+        }
         return 'xx'
+    }
+
+    @Get('/set/:name')
+    async set() {
+        this.session.user = 'zhangsan'
+        return this.session
     }
 }

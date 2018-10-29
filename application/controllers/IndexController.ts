38994b8c499dfda71dbf811d55f6697970c5710e
ranyunlong <549510622@qq.com>
2018-10-29 16:30:40 +0800

    update

--- application/controllers/IndexController.ts
+++ application/controllers/IndexController.ts
@@ -1,21 +1,10 @@
-import { Controller, Get, Session, Database, Server, Header } from '@longjs/server'
+import { Controller, Get } from '@longjs/decorators'
 
 @Controller('/')
 export class IndexController {
-    @Session public session: any
-    @Database('users') public db: Server.Database
-    @Header public headers: any
 
-    @Get
+    @Get('/')
     async index() {
-        if (this.session.user) {
-            return this.session.user
-        }
         return 'xx'
-    }
-
-    @Get
-    async set() {
-        this.session.user = 100
     }
 }

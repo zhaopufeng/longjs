f794c09c62b3e8c9efe146a942fc2e5442225e08
ranyunlong <549510622@qq.com>
2018-11-04 21:59:18 +0800

    update

--- test/controllers/IndexController.ts
+++ test/controllers/IndexController.ts
@@ -1,35 +1,16 @@
 import { Controller, Get, Session, Post, Body, Query, Catch, Headers } from '@longjs/decorators'
-import { TestService } from '../services/TestService'
-import { Database } from '@longjs/database'
-import { CustomHttpException } from '../HttpException/CustomHttpException';
+import { HttpException } from '@longjs/core'
 
 @Controller('/')
 export class IndexController {
     @Session public session: Session;
-    @Database('users') public users: Database;
-    @Database public db: Database;
-
-    constructor(public testService: TestService) {}
 
     @Get('/')
     public async index() {
-        console.log(this.session)
-        // if (!this.session.users) {
-        //     this.session.users = await this.db('users').select()
-        // }
-        // return 'xx'
-    }
-
-    @Get('/user')
-    public async user() {
-        // if (this.session.user) {
-        //     return this.session.user
-        // }
-        return await this.users.select()
+        return 'xx'
     }
 
     @Get
-    @Catch(CustomHttpException)
     public async test(@Headers({
         'content-length': '100'
     }) h: Headers) {
